ec351d88399a43b1e5a568ed904f6ff5f4c8e176
jlee220 <jlee2200@gmail.com>
2022-09-24 00:45:10 -0700

    artist table works

--- db.py
+++ db.py
@@ -1,36 +1,4 @@
-import sqlite3
-
-def create_conn(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except sqlite3.Error as e:
-        print(e)
-    return conn
-
-
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except sqlite3.Error as e:
-        print(e)
-
-
-def main():
-    database = "spotify.db"
-    create_artist_table = """ CREATE TABLE IF NOT EXISTS artist (
+create_artist_table = """ CREATE TABLE IF NOT EXISTS artist (
                                         artist_id varchar(50) PRIMARY KEY,
                                         artist_name varchar(255),
                                         external_url varchar(100),
@@ -43,107 +11,42 @@ def main():
                                     );"""
 
 
-    create_album_table = """ CREATE TABLE IF NOT EXISTS album (
-                                        album_id varchar(50) PRIMARY KEY,
-                                        album_name varchar(255),
-                                        external_url varchar(100),
-                                        image_url varchar(100),
-                                        release_date date,
-                                        total_tracks int,
-                                        type varchar(50),
-                                        album_uri varchar(100),
-                                        artist_id varchar(50),
-                                        FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
-                                    );"""
-    create_track_table = """ CREATE TABLE IF NOT EXISTS track (
-                                        track_id varchar(50) PRIMARY KEY,
-                                        song_name varchar(255),
-                                        external_url varchar(100),
-                                        duration_ms int,
-                                        explicit boolean,
-                                        disc_number int,
-                                        type varchar(50),
-                                        song_uri varchar(100),
-                                        album_id varchar(50),
-                                        FOREIGN KEY (album_id) REFERENCES album (album_id)
-                                    );"""
+create_album_table = """ CREATE TABLE IF NOT EXISTS album (
+                                    album_id varchar(50) PRIMARY KEY,
+                                    album_name varchar(255),
+                                    external_url varchar(100),
+                                    image_url varchar(100),
+                                    release_date date,
+                                    total_tracks int,
+                                    type varchar(50),
+                                    album_uri varchar(100),
+                                    artist_id varchar(50),
+                                    FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
+                                );"""
+create_track_table = """ CREATE TABLE IF NOT EXISTS track (
+                                    track_id varchar(50) PRIMARY KEY,
+                                    song_name varchar(255),
+                                    external_url varchar(100),
+                                    duration_ms int,
+                                    explicit boolean,
+                                    disc_number int,
+                                    type varchar(50),
+                                    song_uri varchar(100),
+                                    album_id varchar(50),
+                                    FOREIGN KEY (album_id) REFERENCES album (album_id)
+                                );"""
 
-    create_track_feature_table = """ CREATE TABLE IF NOT EXISTS track_feature (
-                                        track_id varchar(50) PRIMARY KEY,
-                                        danceability double,
-                                        energy double,
-                                        instrumentalness double,
-                                        liveness double,
-                                        loudness double,
-                                        speechiness double,
-                                        tempo double,
-                                        type varchar(50),
-                                        valence double,
-                                        song_uri varchar(100),
-                                        FOREIGN KEY (song_uri) REFERENCES track (song_uri)
-                                    );"""
-    conn = create_conn(database)
-
-    if conn is not None:
-        create_table(conn, create_album_table)
-        create_table(conn, create_artist_table)
-        create_table(conn, create_track_table)
-        create_table(conn, create_track_feature_table)
-    else:
-        print("Error! Cannot create the db connection.")
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-def insert_artist(conn, artist):
-    insert_artist = '''
-                INSERT INTO artist(artist_name, 
-                external_url, genre, image_url, followers,
-                popularity, type, artist_uri)
-                values (?,?,?,?,?,?,?,?)
-                '''
-    cur = conn.cursor()
-    cur.execute(insert_artist, artist)
-    conn.commit()
-    return cur.lastrowid
-    
-def insert_album(conn, album):
-    insert_album ='''
-                INSERT INTO album(album_name, external_url,
-                image_url, release_date, total_tracks,
-                type, album_uri, artist_uri)
-                values(?,?,?,?,?,?,?,?)
-                '''
-    cur = conn.cursor()
-    cur.execute(insert_album, album)
-    conn.commit()
-    return cur.lastrowid
-
-def insert_track(conn, track):
-    insert_track ='''
-                INSERT INTO track(song_name, external_url,
-                duration_ms, explicit, disc_number, type,
-                song_uri, album_id)
-                values(?,?,?,?,?,?,?,?)
-                '''
-    cur = conn.cursor()
-    cur.execute(insert_track, track)
-    conn.commit()
-    return cur.lastrowid
-
-def insert_track_feature(conn, track_feature):
-        
-    insert_track_feature = '''
-                        INSERT INTO track_feature(danceability,
-                        energy, instrumentalness, liveness,
-                        loudness, speechiness, tempo, type, valence,
-                        song_uri)
-                        values(?,?,?,?,?,?,?,?,?,?)
-                        '''
-    cur = conn.cursor()
-    cur.execute(insert_track_feature, track_feature)
-    conn.commit()
-    return cur.lastrowid
+create_track_feature_table = """ CREATE TABLE IF NOT EXISTS track_feature (
+                                    track_id varchar(50) PRIMARY KEY,
+                                    danceability double,
+                                    energy double,
+                                    instrumentalness double,
+                                    liveness double,
+                                    loudness double,
+                                    speechiness double,
+                                    tempo double,
+                                    type varchar(50),
+                                    valence double,
+                                    song_uri varchar(100),
+                                    FOREIGN KEY (song_uri) REFERENCES track (song_uri)
+                                );"""
--- main.py
+++ main.py
@@ -1,26 +1,304 @@
 import spotipy
-
+import pandas as pd
+import sqlalchemy
 from spotipy.oauth2 import SpotifyClientCredentials
-import db #(create_conn,insert_album, #insert_artist, insert_track, insert_track_feature)
 import sqlite3
+from db import (
+    create_artist_table,
+    create_album_table,
+    create_track_feature_table,
+    create_track_table,
+)
+
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
-database = "spotify.db"
-con = sqlite3.connect("spotify.db")
-
-
-
-lz_uri = "spotify:artist:36QJpDe2go2KgaRleHCDTp"
+engine = sqlalchemy.create_engine("sqlite:///spotify.db")
+conn = sqlite3.connect("spotify.db")
+cursor = conn.cursor()
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
 
-results = spotify.search(q='genre', limit=20, type='artist', market='US')
-items = results['artists']['items']
-for i in items:
-    if len(items) > 0:
-        print(i['name'])
+fav_uri = "spotify:playlist:4v7VBQWmPx6XsoOJaJosWN"
 
-   
+
+artists_id = []
+
+
+def get_artists():
+    # artists_id
+    artist_name = []
+    external_url = []
+    genre = []
+    image_url = []
+    followers = []
+    popularity = []
+    type = []
+    artist_uri = []
+
+    results = spotify.playlist_tracks(fav_uri, limit=20)
+
+    artists = results["items"]
+    for i in range(20):
+        artist_id = artists[i]["track"]["artists"][0]["id"]
+        artists_id.append(artist_id)
+        artist_name.append(artists[i]["track"]["artists"][0]["name"])
+        external_url.append(f"https://open.spotify.com/artist/{artist_id}")
+
+    for artist in artists_id:
+        result = spotify.artist(artist)
+        genre.append(result["genres"][0])
+        image_url.append(result["images"][0]["url"])
+        followers.append(result["followers"]["total"])
+        popularity.append(result["popularity"])
+        type.append(result["type"])
+        artist_uri.append(result["uri"])
+
+    artists_dict = {
+        "artists_id": artists_id,
+        "artist_name": artist_name,
+        "external_url": external_url,
+        "genre": genre,
+        "image_url": image_url,
+        "followers": followers,
+        "popularity": popularity,
+        "type": type,
+        "artist_uri": artist_uri,
+    }
+
+    artists_df = pd.DataFrame(
+        artists_dict,
+        columns=[
+            "artists_id",
+            "artist_name",
+            "external_url",
+            "genre",
+            "image_url",
+            "followers",
+            "popularity",
+            "type",
+            "artist_uri",
+        ],
+    )
+
+    cursor.execute(create_artist_table)
+    try:
+        artists_df.to_sql("artist", engine, index=False, if_exists="append")
+    except:
+        print("Data already exists")
+
+    conn.close()
+    print("close successful")
+
+
+album_id = []
+
+
+def get_artist_albums():
+    # album_id
+    album_name = []
+    external_url = []
+    release_date = []
+    total_tracks = []
+    type = []
+    album_uri = []
+    # artist_id
+
+    for artist in artists_id:
+        results = spotify.artist_albums(
+            artist_id=artist, limit=10, album_type="album", country="US"
+        )
+        albums = results["items"]
+        while results["next"]:
+            results = spotify.next(results)
+            albums.extend(results["items"])
+
+        for album in albums:
+            album_id.append(album["uri"])
+            album_name.append(album["name"])
+            external_url.append(album["external_urls"])
+            release_date.append(album["release_date"])
+            total_tracks.append(album["total_tracks"])
+            type.append(album["type"])
+            album_uri.append(album["uri"])
+
+    album_dict = {
+        "album_id": album_id,
+        "album_name": album_name,
+        "external_url": external_url,
+        "release_date": release_date,
+        "total_tracks": total_tracks,
+        "type": type,
+        "album_uri": album_uri,
+        "artist_id": artists_id,
+    }
+
+    album_df = pd.DataFrame(
+        album_dict,
+        columns=[
+            "album_id",
+            "album_name",
+            "external_url",
+            "release_date",
+            "total_tracks",
+            "type",
+            "album_uri",
+            "artist_id",
+        ],
+    )
+
+    # cursor.execute(create_artist_table)
+    try:
+        album_df.to_sql("artist", engine, index=False, if_exists="append")
+    except:
+        print("Data already exists")
+
+    conn.close()
+    print("close successful")
+
+
+song_uri = []
+
+
+def get_album_tracks():
+    track_id = []
+    song_name = []
+    external_url = []
+    duration_ms = []
+    explicit = []
+    disc_number = []
+    type = []
+    # song_uri
+    # album_id
+
+    for album in album_id:
+        results = spotify.album_tracks(album_id=album, limit=12, market="US")
+        songs = results["items"]
+        for song in songs:
+            track_id.append(song["id"])
+            song_name.append(song["name"])
+            external_url.append(song["external_urls"])
+            duration_ms.append(song["duration_ms"])
+            explicit.append(song["explicit"])
+            disc_number.append(song["disc_number"])
+            type.append(song["type"])
+            song_uri.append(song["uri"])
+
+    tracks_dict = {
+        "track_id": track_id,
+        "song_name": song_name,
+        "external_url": external_url,
+        "duration_ms": duration_ms,
+        "explicit": explicit,
+        "disc_number": disc_number,
+        "type": type,
+        "song_uri": song_uri,
+        "album_id": album_id,
+    }
+
+    tracks_df = pd.DataFrame(
+        tracks_dict,
+        columns=[
+            "track_id",
+            "song_name",
+            "external_url",
+            "duration_ms",
+            "explicit",
+            "disc_number",
+            "type",
+            "song_uri",
+            "album_id",
+        ],
+    )
+
+    # cursor.execute(create_track_table)
+
+    try:
+        tracks_df.to_sql("track", engine, index=False, if_exists="append")
+    except:
+        print("Data already exists")
+
+    conn.close()
+    print("close successful")
+
+
+def get_track_features():
+    track_id = []
+    danceability = []
+    energy = []
+    instrumentalness = []
+    liveness = []
+    loudness = []
+    speechiness = []
+    tempo = []
+    type = []
+    valence = []
+    # song_uri
+
+    for song in song_uri:
+        track = spotify.audio_features(tracks=song)
+        track_id.append(track["id"])
+        danceability.append(track["danceability"])
+        energy.append(track["energy"])
+        instrumentalness.append(track["instrumentalness"])
+        liveness.append(track["liveness"])
+        loudness.append(track["loudness"])
+        speechiness.append(track["speechiness"])
+        tempo.append(track["tempo"])
+        type.append(track["type"])
+        valence.append(track["valence"])
+
+    features_dict = {
+        "track_id": track_id,
+        "danceability": danceability,
+        "energy": energy,
+        "instrumentalness": instrumentalness,
+        "liveness": liveness,
+        "loudness": loudness,
+        "speechiness": speechiness,
+        "tempo": tempo,
+        "type": type,
+        "valence": valence,
+        "song_uri": song_uri,
+    }
+
+    features_df = pd.DataFrame(
+        features_dict,
+        columns=[
+            "track_id",
+            "danceability",
+            "energy",
+            "instrumentalness",
+            "liveness",
+            "loudness",
+            "speechiness",
+            "tempo",
+            "type",
+            "valence",
+            "song_uri",
+        ],
+    )
+
+    # cursor.execute(create_track_feature_table)
+
+    try:
+        features_df.to_sql("track_feature", engine, index=False, if_exists="append")
+    except:
+        print("Data already exists")
+
+    conn.close()
+    print("close successful")
+
+
+def main():
+    get_artists()
+    get_artist_albums()
+    # get_album_tracks()
+    # get_track_features()
+
+
+if __name__ == "__main__":
+    main()
